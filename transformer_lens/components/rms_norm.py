feat(layernorm): fix precision mismatch for ln hook_normalized for rms norm

transformer_lens/components/rms_norm.py
@@ -41,5 +41,6 @@ class RMSNorm(nn.Module):
         scale: Float[torch.Tensor, "batch pos 1"] = self.hook_scale(
             (x.pow(2).mean(-1, keepdim=True) + self.eps).sqrt()
         )
-        x = self.hook_normalized(x / scale  * self.w).to(self.cfg.dtype)  # [batch, pos, length]
+        x = x / scale * self.w
+        x = self.hook_normalized(x.to(self.cfg.dtype))  # [batch, pos, length]
         return x

transformer_lens/components/rms_norm_pre.py
@@ -32,4 +32,6 @@ class RMSNormPre(nn.Module):
         scale: Float[torch.Tensor, "batch pos 1"] = self.hook_scale(
             (x.pow(2).mean(-1, keepdim=True) + self.eps).sqrt()
         )
-        return self.hook_normalized(x / scale).to(self.cfg.dtype)  # [batch, pos, length]
+        x = x / scale
+        x = self.hook_normalized(x.to(self.cfg.dtype))  # [batch, pos, length]
+        return x